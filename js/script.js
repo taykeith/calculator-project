3b817d85765d7f9a2ddb2f408352c21a8c2fc183
taykeith <keith.mbugi@gmail.com>
2019-07-15 14:17:43 +0300

    changed error

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-var   date of birth, gender;
-//Get input
-
-//main caller func
+var   dateofbirth, gender;
 
 //arrays
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["akosua", "adwoa", "abenna", "akua", "yaa", "afua", "ama
+let femaleNames = ["akosua", "adwoa", "abenna", "akua", "yaa", "afua", "ama"];
