2002b8ae784361f4f462c5de9e3fad70c734f7ae
taykeith <keith.mbugi@gmail.com>
2019-07-15 15:45:10 +0300

    changed input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var   dateofbirth, gender;
+var  dateofbirth, gender;
 
 //arrays
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["akosua", "adwoa", "abenna", "akua", "yaa", "afua", "ama"];
+
+function displayName(){
+for(i in maleNames){
+  var output = document.getElementById('output').innerHTML = maleNames[i];
+  alert(maleNames[i]);
+}
+  return output;
+}
